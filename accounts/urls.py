5ded4775562df0ad7e70d3e4c7f035a8e9e3e4d9
David <david@david-Lenovo-U310>
2017-01-14 22:06:34 -0600

    se quito /login en url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,18 +3,16 @@ from . import views
 from django.contrib.auth.views import login, logout
 
 urlpatterns = [
-	url(r'^login/$', login, name="login"),
+	
 	url(r'^logout/$', logout, name="logout"),
-	#url(r'^login/$', views.LoginView.login, name="login"),
-	#url(r'^logout/$', views.LogoutView.logout, name="logout"),
+
 	url(r'^profile/$', views.Profile.as_view(), name="Profile"),
 
 	url(r'^accounts/nuevo/$', views.CreateViewAccount.as_view(), name="CreateViewAccount"),
 	url(r'^accounts/actualizar/(?P<pk>\d+)/$', views.UpdateViewAccount.as_view(), name="UpdateViewAccount"),
-
 	url(r'^accounts/eliminar/(?P<pk>\d+)/$', views.DeleteViewAccount.as_view(), name="DeleteViewAccount"),
-
 	url(r'^accounts/(?P<pk>\d+)/$', views.DetailViewAccount.as_view(), name="DetailViewAccount"),
 	url(r'^accounts', views.ListViewAccounts.as_view(),name="ListViewAccounts"),
+	url(r'^', login, name="login"),
 ]
 
